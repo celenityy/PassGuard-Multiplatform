7cee6d9e44f2fa363f574a09a9d6f1d39ad8dd18
Andrew <thejohnsondev@gmail.com>
2025-10-11 12:19:47 +0300

    fix(general): remove blur where not supported

--- core/ui/src/commonMain/kotlin/com/thejohnsondev/ui/scaffold/HomeScaffold.kt
+++ core/ui/src/commonMain/kotlin/com/thejohnsondev/ui/scaffold/HomeScaffold.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
